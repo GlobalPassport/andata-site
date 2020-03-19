5be3aab5d52bdc557e731d71b8aa2cdf3750bfad
Danil Ishutin <danil.g.ishutin@yandex.ru>
2020-03-20 02:03:09 +0300

    layout completed front page

---
+++ assets/js/site.js
@@ -0,0 +1,9 @@
+$(function(){
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev'
+    });
+});
