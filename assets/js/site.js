7d06b300f98588e9261c2818cf490e10e8805fc1
Danil Ishutin <danil.g.ishutin@yandex.ru>
2020-03-31 12:14:22 +0300

    31.03.2020

---
+++ assets/js/site.js
@@ -0,0 +1,13 @@
+$(function(){
+    var mySwiper = new Swiper ('.swiper-container', {
+        loop: true,
+        pagination: '.swiper-pagination',
+        paginationClickable: true,
+        nextButton: '.swiper-button-next',
+        prevButton: '.swiper-button-prev',
+        width: 280,
+        updateOnWindowResize: true
+        // height: 90
+    });
+});
+
